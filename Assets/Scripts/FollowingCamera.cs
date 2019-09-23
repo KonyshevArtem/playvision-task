bdd52993e9b5e02e53719752d44c14ec57eae063
Artem Konyshev <konyshev_arte@mail.ru>
2019-09-23 21:48:43 +0500

    Add prefabs; Add FollowingCamera; Add GravityController
    - Add Ramp1, Ramp2 prefabs
    - Add game preview screenshot
    - Add FollowingCamera class to follow target with given offset
    - Add GravityController class to control direction of gravity force
    - Add physics sphere on main scene

---
+++ Assets/Scripts/FollowingCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+    [SerializeField]
+    private Vector3 offset;
+
+    private void Update()
+    {
+        transform.position = target.transform.position + offset;
+        transform.rotation = Quaternion.LookRotation(target.transform.position - transform.position);
+    }
+}
---
+++ Assets/Scripts/GravityController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GravityController : MonoBehaviour, IDragHandler
+{
+    [SerializeField] private float gravityMultiplier;
+
+    private Vector3 gravityDirection;
+
+    private void Start()
+    {
+        gravityDirection = Physics.gravity.normalized;
+    }
+
+    private void Update()
+    {
+        Physics.gravity = gravityDirection * gravityMultiplier;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 normalizedDelta = eventData.delta.normalized;
+        gravityDirection += new Vector3(normalizedDelta.x, 0, normalizedDelta.y) * Time.deltaTime;
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (gravityDirection.sqrMagnitude > 0)
+        {
+            Handles.ArrowHandleCap(0, Camera.main.transform.position, Quaternion.LookRotation(gravityDirection), 10,
+                EventType.Repaint);
+        }
+    }
+#endif
+}
