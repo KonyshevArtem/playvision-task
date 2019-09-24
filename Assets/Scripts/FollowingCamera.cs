83ca4c8ade64c828c3f4a70d4588a261721e558a
Artem Konyshev <konyshev_arte@mail.ru>
2019-09-24 19:08:44 +0500

    Refactor; Add LevelController; Add LevelRotator
    - Add RotateCameraToTarget and PlaceCameraToTarget methods to FollowingCamera class
    - Add summaries
    - Add LevelController class for reloading current scene
    - Remove GravityController class
    - Add LevelRotator class for rotating geometry around ball
    - Add reload button to main scene
    - Make background solid color in main scene

--- Assets/Scripts/FollowingCamera.cs
+++ Assets/Scripts/FollowingCamera.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// This class controls position of game camera.
+/// </summary>
 public class FollowingCamera : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject target;
-    [SerializeField]
-    private Vector3 offset;
+    [SerializeField] private GameObject target;
+    [SerializeField] private Vector3 offset;
+
 
     private void Update()
     {
+        PlaceCameraToTarget();
+        RotateCameraToTarget();
+    }
+
+    /// <summary>
+    /// Set position of camera to target position with offset.
+    /// </summary>
+    private void PlaceCameraToTarget()
+    {
         transform.position = target.transform.position + offset;
-        transform.rotation = Quaternion.LookRotation(target.transform.position - transform.position);
+    }
+
+    /// <summary>
+    /// Rotate camera to face target.
+    /// </summary>
+    private void RotateCameraToTarget()
+    {
+        Vector3 lookDirection = target.transform.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(lookDirection);
     }
 }
---
+++ Assets/Scripts/LevelRotator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// This class receives drag input and rotates level to drag delta.
+/// </summary>
+public class LevelRotator : MonoBehaviour, IDragHandler
+{
+    [SerializeField] private GameObject level;
+    [SerializeField] private GameObject pivot;
+    [SerializeField] private Vector2 rotationSensitivity;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        RotateLevel(eventData.delta);
+    }
+
+    /// <summary>
+    /// Rotate level geometry around pivot.
+    /// </summary>
+    /// <param name="delta">Rotation angle delta</param>
+    private void RotateLevel(Vector2 delta)
+    {
+        level.transform.RotateAround(pivot.transform.position, Vector3.back, delta.x * rotationSensitivity.x);
+        level.transform.RotateAround(pivot.transform.position, Vector3.right, delta.y * rotationSensitivity.y);
+    }
+}
